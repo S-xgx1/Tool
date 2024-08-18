b06e1b947189ab399eef9aeed4e7473682064ef6
s_xgx1 <S_xgx1@outlook.com>
2024-08-18 16:16:39 +0800

    Initial commit

---
+++ Tool/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+string directoryPath = args[0];
+string[] files = Directory.GetFiles(directoryPath,"", SearchOption.AllDirectories);
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+foreach (string filePath in files)
+{
+    string content = File.ReadAllText(filePath, Encoding.GetEncoding("gbk"));
+    File.WriteAllText(filePath, content, Encoding.UTF8);
+}
